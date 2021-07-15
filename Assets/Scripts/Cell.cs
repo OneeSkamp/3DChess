37da784c47e874917ed87f007001e7dce935117e
Egor Zalevski <oneeskamp@gmail.com>
2021-07-15 23:41:24 +0300

    resolve conflicts

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-
 public class Cell : MonoBehaviour {
     public GameObject figure;
+    public string color;
+    public int xPos;
+    public int yPos;
+    public bool canMove = false;
 }
