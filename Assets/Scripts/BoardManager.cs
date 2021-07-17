50946af237d61da61f6137ea39ae96ec8c721e02
Egor Zalevski <oneeskamp@gmail.com>
2021-07-17 19:17:22 +0300

    code refactoring

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,16 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class BoardManager : MonoBehaviour {
     [SerializeField] private GameObject board;
     [SerializeField] private Material yelow;
     [SerializeField] private Material black;
     [SerializeField] private Material white;
+    [SerializeField] private Material blue;
+
     private GameObject activeCell;
-    private GameObject lastActiveCell;
-    private GameObject moveCell;
     private ChessControl chessAction;
     private GameObject[,] gridArray;
     private Vector3 mousePosition = new Vector3();
@@ -18,9 +15,6 @@ public class BoardManager : MonoBehaviour {
     private void Awake() {
         chessAction = new ChessControl();
         chessAction.Mouse.Click.performed += ctx => OnClick();
-        chessAction.Mouse.Click.performed += ctx => GetMovePawnMap();
-        
-        chessAction.Mouse.Click.performed += ctx => MoveFigure();
         chessAction.Mouse.Click.performed += ctx => Render();
     }
 
@@ -32,9 +26,6 @@ public class BoardManager : MonoBehaviour {
         chessAction.Disable();    
     }
 
-    private void Start(){
-        
-    }
     private void OnClick() {
         gridArray = board.GetComponent<BoardBuilder>().gridArray;
         mousePosition = chessAction.Mouse.MousePosition.ReadValue<Vector2>();
@@ -42,37 +33,63 @@ public class BoardManager : MonoBehaviour {
         Ray ray = Camera.main.ScreenPointToRay(mousePosition);
         RaycastHit hitInfo;
 
-        if (Physics.Raycast(ray, out hitInfo)){
+        if (Physics.Raycast(ray, out hitInfo)) {
 
-            if (activeCell != null) {
-                lastActiveCell = activeCell;
-                lastActiveCell.GetComponent<Cell>().active = false;
+            if (activeCell == null) {
                 activeCell = hitInfo.collider.gameObject;
                 activeCell.GetComponent<Cell>().active = true;
-                
-                Debug.Log(lastActiveCell.name);
-
-                Debug.Log(activeCell.name);
-
-            } else {
-                activeCell = hitInfo.collider.gameObject;
-                lastActiveCell = activeCell;
-                activeCell.GetComponent<Cell>().active = true;
-                Debug.Log(activeCell.name);
+                if (activeCell.GetComponent<Cell>().figure != null) {
+                    GetFigureType(activeCell.GetComponent<Cell>().figure.GetComponent<Figure>().type);
+                }
             }
-        }
+            else if (activeCell != null && hitInfo.collider.gameObject.GetComponent<Cell>().figure != null) {
+                activeCell.GetComponent<Cell>().active = false;
+                activeCell = hitInfo.collider.gameObject;
+                activeCell.GetComponent<Cell>().active = true;
 
-        Debug.Log(gridArray);       
+                if (activeCell.GetComponent<Cell>().figure != null) {
+                    GetFigureType(activeCell.GetComponent<Cell>().figure.GetComponent<Figure>().type);
+                }
+            }
+            else if (activeCell != null && hitInfo.collider.gameObject.GetComponent<Cell>().canMove) {
+                CleaningCanMoveCells();
+                MoveFigure(hitInfo.collider.gameObject);
+                activeCell.GetComponent<Cell>().active = false;
+                activeCell = null;
+            }
+        }     
+    }
+
+    private void CleaningCanMoveCells() {
+
+        foreach (GameObject cell in gridArray) {
+            cell.GetComponent<Cell>().canMove = false;    
+        }
+    }
+
+    private void GetFigureType(string figureType) {
+        
+        CleaningCanMoveCells();
+        
+        switch (figureType) {
+            case "BPawn":
+                Debug.Log("GetBpawn");
+                GetMovePawnMap();
+                break;
+        }
     }
 
     private void CleaningBoard() {
         var figures = GameObject.FindGameObjectsWithTag( "Figure" ); 
-        for( int i = 0; i < figures.Length; i++ ){
-            Destroy( figures[ i ] );  
+        for(int i = 0; i < figures.Length; i++){
+            Destroy( figures[i] );  
         }      
     }
 
     private void Render() {
+        
+        CleaningBoard();
+        
         foreach(GameObject cell in gridArray) {
             if (cell.GetComponent<Cell>().active) {
                 cell.GetComponent<Renderer>().material = yelow;
@@ -83,64 +100,48 @@ public class BoardManager : MonoBehaviour {
                 if (cell.GetComponent<Cell>().baceColor == "White") {
                     cell.GetComponent<Renderer>().material = white;     
                 }
+                if (cell.GetComponent<Cell>().canMove) {
+                    cell.GetComponent<Renderer>().material = blue;
+                }
             }
 
-            if (cell.GetComponent<Cell>().figure != null) {
-                
+            if (cell.GetComponent<Cell>().figure != null) {               
                 GameObject figure = Instantiate(cell.GetComponent<Cell>().figure);
                 figure.transform.position = new Vector3(cell.transform.position.x, 0.5f, cell.transform.position.z);
             }
         } 
     }
 
-    private void MoveFigure() {
-        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-        RaycastHit hitInfo;
-
-        if (Physics.Raycast(ray, out hitInfo))
-        {
-            if (hitInfo.collider.gameObject.GetComponent<Cell>().canMove)
-            {
-                Debug.Log("movefigure");
-                moveCell = activeCell;
-                moveCell.GetComponent<Cell>().figure = lastActiveCell.GetComponent<Cell>().figure;
-                lastActiveCell.GetComponent<Cell>().figure = null;
-                activeCell.GetComponent<Cell>().active = false;
-                activeCell.GetComponent<Cell>().canMove = false;
-                activeCell = null;
-                lastActiveCell = null;
-                moveCell = null;
-                CleaningBoard();                
-            }
-        }
+    private void MoveFigure(GameObject cellForMove) {
+        cellForMove.GetComponent<Cell>().figure = activeCell.GetComponent<Cell>().figure;
+        activeCell.GetComponent<Cell>().figure = null;
     }
 
     private void GetMovePawnMap() {
-        
-        if (lastActiveCell.GetComponent<Cell>().figure != null && lastActiveCell.GetComponent<Cell>().figure.GetComponent<Figure>().type == "BPawn") {
-            //moveCell = activeCell;
-            GameObject figure = lastActiveCell.GetComponent<Cell>().figure;
 
-            if (figure.GetComponent<Figure>().step == 1 && lastActiveCell != null) {
+        GameObject figure = activeCell.GetComponent<Cell>().figure;           
 
-                int step = figure.GetComponent<Figure>().step;
+        if (figure.GetComponent<Figure>().step == 1 && activeCell != null) {
 
-                int posX = lastActiveCell.GetComponent<Cell>().xPos;
-                int posY = lastActiveCell.GetComponent<Cell>().yPos;
-                Debug.Log($"{posX + 1} {posY}");
-                if (gridArray[posX + 1, posY].GetComponent<Cell>().figure == null)
-                {                   
-                    gridArray[posX + 1, posY].GetComponent<Cell>().canMove = true;
-                }
+            int step = figure.GetComponent<Figure>().step;
 
-                if (step == 1)
-                {
-                    if (gridArray[posX + 2, posY].GetComponent<Cell>().figure == null)
-                    {
-                        gridArray[posX + 2, posY].GetComponent<Cell>().canMove = true;
-                    }
-                } 
+            int posX = activeCell.GetComponent<Cell>().xPos;
+            int posY = activeCell.GetComponent<Cell>().yPos;
+
+            Debug.Log($"{posX + 1} {posY}");
+
+            if (gridArray[posX + 1, posY].GetComponent<Cell>().figure == null)
+            {                   
+                gridArray[posX + 1, posY].GetComponent<Cell>().canMove = true;
             }
+
+            if (step == 1)
+            {
+                if (gridArray[posX + 2, posY].GetComponent<Cell>().figure == null)
+                {
+                    gridArray[posX + 2, posY].GetComponent<Cell>().canMove = true;
+                }
+            } 
         }
     }
 }
